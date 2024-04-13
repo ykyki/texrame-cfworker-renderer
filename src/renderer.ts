4f427790b072d550dfcafc0e6d77cfa437aab06d
ykyki <bsyki@pm.me>
2024-04-13 15:21:20 +0900

    reuse session for puppeteer browser

--- src/renderer.ts
+++ src/renderer.ts
@@ -46,14 +46,55 @@ export const renderer = async (
         `,
     )) as Buffer;
 
-    browser.close();
+    teardownBrowser(browser);
 
     return svg;
 };
 
 const setupBrowser = async (env: Env): Promise<puppeteer.Browser> => {
     // @ts-ignore
-    const browser = await puppeteer.launch(env.MY_BROWSER);
+    const sessionId = await retrieveSession(env.MY_BROWSER);
 
+    if (sessionId !== null) {
+        try {
+            // @ts-ignore
+            const browser = await puppeteer.connect(env.MY_BROWSER, sessionId);
+            return browser;
+        } catch (e) {
+            throw new Error("Failed to connect to the browser");
+        }
+    }
+
+    // @ts-ignore
+    const browser = await puppeteer.launch(env.MY_BROWSER);
     return browser;
+};
+
+const teardownBrowser = async (browser: puppeteer.Browser): Promise<void> => {
+    browser.disconnect();
+};
+
+const retrieveSession = async (
+    endpoint: puppeteer.BrowserWorker,
+): Promise<string | null> => {
+    const sessions: puppeteer.ActiveSession[] =
+        await puppeteer.sessions(endpoint);
+
+    const sessionsIds = sessions
+        .filter((v) => {
+            return v.connectionId === undefined; // remove sessions with workers connected to them
+        })
+        .map((v) => {
+            return v.sessionId;
+        });
+
+    if (sessionsIds.length === 0) {
+        return null;
+    }
+
+    // pick a random session
+    const sessionId =
+        sessionsIds[Math.floor(Math.random() * sessionsIds.length)];
+
+    return sessionId;
 };
